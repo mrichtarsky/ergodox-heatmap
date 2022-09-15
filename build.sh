656a5a41483c4e032b355bc56f63e3ab911f97ec
Martin Richtarsky <s@martinien.de>
2022-09-15 11:51:35 +0200

    Improve build

--- build.sh
+++ build.sh
@@ -9,17 +9,23 @@ SCRIPT_PATH=$(cd "$SCRIPT_PATH" && pwd)
 # Supposed to be under version control
 KEYMAP=c:/projects/qmk_firmware/keyboards/ergodox_ez/keymaps/martin_colemak
 
-if [ $(cd $KEYMAP; git status --porcelain keymap.c | grep '??' | wc -l) -ne "0" ]; then
+if [ $(cd $KEYMAP; git status --porcelain keymap.c | grep ' M' | wc -l) -ne "0" ]; then
   echo "Unstaged changes, quitting"
   exit 1
 fi
 
-patch $KEYMAP/keymap.c $SCRIPT_PATH/trace_codes.patch
+patch -N $KEYMAP/keymap.c $SCRIPT_PATH/trace_codes.patch
+
+trap cleanup EXIT
+
+function cleanup()
+{
+    pushd $KEYMAP
+    git checkout keymap.c
+    popd
+}
+
 python c:/projects/ergodox-compress-keymap/ergodox_compress_keymap.py
 cd c:/projects/qmk_firmware
 qmk compile -kb ergodox_ez/glow -km martin_colemak
 "C:/Program Files (x86)/Wally/Wally.exe" c:/projects/qmk_firmware/.build/ergodox_ez_glow_martin_colemak.hex
-
-pushd $KEYMAP
-git checkout keymap.c
-popd
